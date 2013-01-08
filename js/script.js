8b57b971e16dd481147751db1c10fdd43b86dd1a
Garrett Samm <garrett@garrettsamm.com>
2013-01-08 14:26:11 -0600

    Images are now pulled from Instagram instead of Flickr.
    Removed references to Flickr in object names.  Instagram API query
    finds images tagged with the lat/lon of Winnipeg.

--- js/script.js
+++ js/script.js
@@ -1,80 +1,100 @@
 (function () {
-	var Gallery = {};
+	// Namespaces
+	var Gallery = {
+		Models: {},
+		Collections: {},
+		Views: {},
 
-	Gallery.IMAGES = 25;
-	Gallery.IMAGES_PER_ROW = 5;
-	Gallery.TILE_WIDTH = Gallery.TILE_HEIGHT = 75;
+		// Constants
+		IMAGES: 36,
+		IMAGES_PER_ROW: 6,
+		TILE_WIDTH: 51,
+		TILE_HEIGHT: 51,
+	};
 
-	// PubSub object to facilitate global events.
-	Gallery.Events = _.extend({}, Backbone.Events);
+	// Facilitate global events.
+	_.extend(Gallery, Backbone.Events);
 
-	Gallery.Models = {};
-	Gallery.Models.FlickrPhoto = Backbone.Model.extend({});
+	// Models
+	Gallery.Models.Photo = Backbone.Model.extend({});
 
-	Gallery.Collections = {};
-	Gallery.Collections.FlickrPhotoCollection = Backbone.Collection.extend({
-		model: Gallery.Models.FlickrPhoto,
-		url: "http://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=1d562390dd25190883b74f6b83bdd74e&extras=url_sq%2C+url_n&per_page=" + Gallery.IMAGES + "&format=json&nojsoncallback=1",
+	// Collections
+	Gallery.Collections.PhotoCollection = Backbone.Collection.extend({
+		model: Gallery.Models.Photo,
+		// url: "http://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=1d562390dd25190883b74f6b83bdd74e&extras=url_sq%2C+url_n&per_page=" + Gallery.IMAGES + "&format=json&nojsoncallback=1",
+		url: 'https://api.instagram.com/v1/media/popular?client_id=f16cbf170e71470583ae9bec19f97ab3&count=' + Gallery.IMAGES,
 
 		parse: function (response) {
-			return response.photos.photo;
+			return response.data;
 		}
 	});
 
-	Gallery.Views = {};
-	
+	// Views
 	Gallery.Views.SpinnerView = {
 		show: function () {
-			$("#spinner").css("display", "block");
+			$('#spinner').css('display', 'block');
 		},
 
 		hide: function () {
-			$("#spinner").css("display", "none");
+			$('#spinner').css('display', 'none');
 		}
 	};
 
-	Gallery.Views.FlickrPhotoView = Backbone.View.extend({
-		tagName: "li",
-		template: $("#gallery-item-template").html(),
+	Gallery.Views.PhotoView = Backbone.View.extend({
+		tagName: 'li',
+		template: $('#gallery-item-template').html(),
+
+		// attributes: {
+		// 	'class': function () {
+		// 		var lastRowIndex
+		// 		if (this.row === ((Gallery.IMAGES / Gallery.IMAGES_PER_ROW) - 1)) {
+
+		// 		}
+		// 	}
+		// },
 
 		events: {
-			"click .tile": "tileClicked",
-			"mouseenter .front": "fadeIn",
-			"mouseleave .front": "fadeOut"
+			'click .tile': 'tileClicked',
+			'mouseenter .front': 'fadeIn',
+			'mouseleave .front': 'fadeOut'
 		},
 
 		initialize: function () {
 			this.row = this.options.row;
 			this.col = this.options.col;
 
-			Gallery.Events.on("tile:flipToFront", this.flipToFront, this);
-			Gallery.Events.on("tile:flipToBack", this.flipToBack, this);
+			Gallery.on('tile:flipToFront', this.flipToFront, this);
+			Gallery.on('tile:flipToBack', this.flipToBack, this);
+
+			this.model.set('imageUrl', this.model.get('images').low_resolution.url);
 		},
 
 		render: function () {
-			var img = $("<img />").attr("src", this.model.get("url_sq")).load(function () {
-				Gallery.Events.trigger("image:loaded");
+			var img = $('<img />').attr('src', this.model.get('imageUrl')).load(function () {
+				Gallery.trigger('image:loaded');
 			});
 
 			$(this.el).html(this.template);
-			$(this.el).find(".front").append(img);
+			$(this.el).find('.front').append(img);
 
 			return this;
 		},
 
 		tileClicked: function (event) {
 			if (this.flipped) {
-				Gallery.Events.trigger("tile:flipToFront", this.row, this.col);
+				Gallery.trigger('tile:flipToFront', this.row, this.col);
 			} else {
 				var that = this;
-				var imageUrl = this.model.get("url_n");
+				var imageUrl = this.model.get('imageUrl');
 
-				console.log("Started downloading: " + imageUrl);
+				console.log('Started downloading: ' + imageUrl);
+
 				Gallery.Views.SpinnerView.show();
-				$("<img />").attr("src", imageUrl).load(function () {
-					console.log("Finished downloading: " + imageUrl);
+				
+				$('<img />').attr('src', imageUrl).load(function () {
+					console.log('Finished downloading: ' + imageUrl);
 					Gallery.Views.SpinnerView.hide();
-					Gallery.Events.trigger("tile:flipToBack", imageUrl, that.row, that.col);
+					Gallery.trigger('tile:flipToBack', imageUrl, that.row, that.col);
 				});
 			}
 		},
@@ -86,7 +106,7 @@
 			var delay = 100 * (distanceX + distanceY);
 
 			setTimeout(function () {
-				that.$(".tile").toggleClass("flipped");
+				that.$('.tile').toggleClass('flipped');
 				that.flipped = !that.flipped;
 			}, delay);
 		},
@@ -96,54 +116,53 @@
 		},
 
 		flipToBack: function (imageUrl, row, col) {
-			var tileBack = this.$(".back");
+			var tileBack = this.$('.back');
 			var backgroundPositionX = this.col * Gallery.TILE_WIDTH;
 			var backgroundPositionY = this.row * Gallery.TILE_HEIGHT;
 
-			tileBack.css("background-image", "url(" + imageUrl + ")");
-			tileBack.css("background-position", "-" + backgroundPositionX + "px -" + backgroundPositionY + "px");
+			tileBack.css('background-image', 'url(' + imageUrl + ')');
+			tileBack.css('background-position', '-' + backgroundPositionX + 'px -' + backgroundPositionY + 'px');
 			
 			this.flip(row, col);
 		},
 
 		fadeIn: function (event) {
-			this.$(".front").stop().animate({ opacity: "1" }, 500);
+			this.$('.front').stop().animate({ opacity: '1' }, 500);
 		},
 
 		fadeOut: function (event) {
-			this.$(".front").stop().animate({ opacity: "0.5" }, 500);
+			this.$('.front').stop().animate({ opacity: '0.5' }, 500);
 		}
 	});
 
-	Gallery.Views.FlickrPhotoCollectionView = Backbone.View.extend({
-		template: Handlebars.compile($("#gallery-template").html()),
+	Gallery.Views.PhotoCollectionView = Backbone.View.extend({
+		template: Handlebars.compile($('#gallery-template').html()),
 
 		initialize: function () {
 			var that = this;
 
 			this.imagesLoaded = 0;
-			this.flickrPhotoViews = [];
-			this.flickrPhotoReflectionViews = [];
+			this.photoViews = [];
 
-			this.collection.each(function (flickrPhoto, i) {
-				that.flickrPhotoViews.push(new Gallery.Views.FlickrPhotoView({
-					model: flickrPhoto,
+			var lastRowIndex = Gallery.IMAGES / Gallery.IMAGES_PER_ROW - 1;
+
+			this.collection.each(function (photo, i) {
+				var row = Math.floor(i / Gallery.IMAGES_PER_ROW);
+				var className = '';
+
+				if (row === lastRowIndex) {
+					className = 'reflection';
+				}
+
+				that.photoViews.push(new Gallery.Views.PhotoView({
+					className: className,
+					model: photo,
 					row: Math.floor(i / Gallery.IMAGES_PER_ROW),
 					col: i % Gallery.IMAGES_PER_ROW
 				}));
 			});
 
-			var lastRowIndex = (Gallery.IMAGES / Gallery.IMAGES_PER_ROW - 1) * Gallery.IMAGES_PER_ROW;
-
-			for (var i = lastRowIndex; i < Gallery.IMAGES; i++) {
-				this.flickrPhotoReflectionViews.push(new Gallery.Views.FlickrPhotoView({
-					model: this.collection.at(i),
-					row: Math.floor(i / Gallery.IMAGES_PER_ROW),
-					col: i % Gallery.IMAGES_PER_ROW
-				}));
-			};
-
-			Gallery.Events.on("image:loaded", this.imageLoaded, this);
+			Gallery.on('image:loaded', this.imageLoaded, this);
 
 			this.render();
 		},
@@ -151,14 +170,10 @@
 		render: function () {
 			var that = this;
 
-			$(this.el).css("width", Gallery.IMAGES_PER_ROW * Gallery.TILE_WIDTH);
+			$(this.el).css('width', Gallery.IMAGES_PER_ROW * Gallery.TILE_WIDTH);
 
-			_(this.flickrPhotoViews).each(function (flickrPhotoView) {
-				$(that.el).append(flickrPhotoView.render().el);
-			});
-
-			_(this.flickrPhotoReflectionViews).each(function (flickrPhotoView) {
-				$("#reflections").append(flickrPhotoView.render().el);
+			_(this.photoViews).each(function (photoView) {
+				$(that.el).append(photoView.render().el);
 			});
 
 			return this;
@@ -166,10 +181,10 @@
 
 		imageLoaded: function () {
 			this.imagesLoaded += 1;
-			console.log("Images loaded:" + this.imagesLoaded);
+			console.log('Images loaded:' + this.imagesLoaded);
 
 			if (this.imagesLoaded === Gallery.IMAGES) {
-				console.log("All images loaded.");
+				console.log('All images loaded.');
 				Gallery.Views.SpinnerView.hide();
 				this.show();
 			}
@@ -183,15 +198,15 @@
 	});
 
 	$(function () {
-		var flickrPhotoCollection = new Gallery.Collections.FlickrPhotoCollection();
+		var photoCollection = new Gallery.Collections.PhotoCollection();
 
 		Gallery.Views.SpinnerView.show();
-		flickrPhotoCollection.fetch({
-			dataType: "json",
+		photoCollection.fetch({
+			dataType: 'jsonp',
 			success: function (collection, response) {
-				new Gallery.Views.FlickrPhotoCollectionView({ 
-					collection: flickrPhotoCollection,
-					el: $("#gallery")
+				new Gallery.Views.PhotoCollectionView({ 
+					collection: photoCollection,
+					el: $('#gallery')
 				});
 			}
 		});
